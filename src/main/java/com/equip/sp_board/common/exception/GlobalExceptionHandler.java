49a04743579fd5e657ed1343ca9b8fb5efbf8dfd
haily <49448330+melly8954@users.noreply.github.com>
2025-10-08 21:37:10 +0900

    feat: 전역 예외 처리 핸들러 추가

---
+++ src/main/java/com/equip/sp_board/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.equip.sp_board.common.exception;
+
+
+import com.equip.sp_board.common.controller.ResponseController;
+import com.equip.sp_board.common.dto.ResponseDto;
+import com.equip.sp_board.common.trace.RequestTraceIdFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler implements ResponseController {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ResponseDto<Void>> handleCustomException(CustomException e) {
+        String traceId = RequestTraceIdFilter.getTraceId();
+        ErrorType errorType = e.getErrorType();
+        log.error("TraceId: {}, 비즈니스 로직 예외 발생 - Code: {}, Message: {}",
+                traceId, errorType.getErrorCode(), errorType.getMessage());
+
+        return makeResponseEntity(
+                traceId,
+                errorType.getStatus(),
+                errorType.getErrorCode(),
+                errorType.getMessage(),
+                null
+        );
+    }
+
+}
