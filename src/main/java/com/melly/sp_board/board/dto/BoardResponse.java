60ec3db0c286b7a68c568e4492a0bb53925b9ca8
haily <49448330+melly8954@users.noreply.github.com>
2025-10-16 23:03:52 +0900

    fix: isAdmin 필드 추가 및 owner, admin 필드 JSON 직렬화 제외

--- src/main/java/com/melly/sp_board/board/dto/BoardResponse.java
+++ src/main/java/com/melly/sp_board/board/dto/BoardResponse.java
@@ -1,6 +1,8 @@
 package com.melly.sp_board.board.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.melly.sp_board.filestorage.dto.FileDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,13 +14,17 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties({"owner", "admin"})
 public class BoardResponse {
     private Long boardId;
     private String boardType;
     private String title;
     private String content;
     private String writerName;
+    @JsonProperty("isOwner")
     private boolean isOwner;
+    @JsonProperty("isAdmin")
+    private boolean isAdmin;
     private Integer viewCount;
     private Integer likeCount;
     private List<FileDto> files;
